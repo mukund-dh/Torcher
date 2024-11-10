4e37a25afcd2153f22cdd2f01f60976906b52e5b
Mukund Dhananjay <mukund.dh@outlook.com>
2024-11-10 18:28:58 +0530

    Working Tokenizer

---
+++ Source/Torcher/Private/MakeMore/MMTokenizer.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MakeMore/MMTokenizer.h"
+#include "Misc/FileHelper.h"
+
+TORCH_INCLUDES_START
+#include "torch/csrc/autograd/generated/variable_factories.h"
+TORCH_INCLUDES_END
+
+bigramLookups UMMTokenizer::bgl;
+
+UMMTokenizer::UMMTokenizer() : FilePath("")
+{
+}
+
+
+void UMMTokenizer::GetWordsList()
+{
+	FString FileContent;
+	if (FFileHelper::LoadFileToString(FileContent, *FilePath))
+	{
+		FileContent.ParseIntoArrayLines(WordsList);		
+	}
+
+	at::Tensor bgCounts = GetBigramCounts();
+	UE_LOG(LogTemp, Warning, TEXT("The bigram value at 4,5 is %d"), bgCounts[4][5].item<int32>());
+}
+
+at::Tensor UMMTokenizer::GetBigramCounts() const noexcept
+{
+	auto opt = at::TensorOptions().dtype(at::ScalarType::Int);
+	
+	at::Tensor CountTensor = torch::zeros({27, 27}, opt);
+
+	for (FString Word : WordsList)
+	{
+		Word = "." + Word + ".";
+		TArray<TCHAR> CharArr = Word.GetCharArray();
+
+		for (size_t i = 0; i < CharArr.Num() - 1; ++i)
+		{
+			TCHAR ch1 = CharArr[i];
+			TCHAR ch2 = CharArr[i + 1];
+			int32 ix1 = bgl.getIndexOfChar(ch1);
+			int32 ix2 = bgl.getIndexOfChar(ch2);
+			CountTensor[ix1][ix2] += 1;
+		}
+	}
+	
+	return CountTensor;
+}
---
+++ Source/Torcher/Public/MakeMore/MMTokenizer.h
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Containers/StaticArray.h"
+#include "UObject/Object.h"
+#include "Macros/TorcherPreProcMacros.h"
+
+TORCH_INCLUDES_START
+#include <torch/csrc/autograd/function.h>
+TORCH_INCLUDES_END
+
+#include "MMTokenizer.generated.h"
+
+
+struct bigramLookups
+{
+private:
+	TCHAR letters[27];
+
+public:
+
+	bigramLookups()
+	{
+		letters[0] = '.';
+		for (int i = 0; i < 26; i++)
+		{
+			letters[i+1] = 'a' + i;
+		}
+	}
+
+	TCHAR getCharAtIndex(int32 index) const
+	{
+		return ((0 <= index) && (index <=26)) ? letters[index] : NULL;
+	}
+
+	int32 getIndexOfChar(char inChar, int32 startIdx = 0, int32 endIdx = 25) const
+	{
+		if (startIdx >= endIdx)
+			return -1;
+
+		int32 index = -1;
+		int32 pivot = startIdx + (endIdx - startIdx) / 2;
+		if (inChar == letters[pivot])
+			return pivot;
+		
+		if (inChar < letters[pivot])
+		{
+			return getIndexOfChar(inChar, startIdx, pivot);
+		}
+		else
+		{
+			return getIndexOfChar(inChar, pivot + 1, endIdx);
+		}
+	}
+};
+
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class TORCHER_API UMMTokenizer : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category="Tokens|Files")
+	FString FilePath;
+
+public:
+
+	UMMTokenizer();
+
+	UFUNCTION(BlueprintCallable, Category="MMTokenizer|FileOps")
+	void GetWordsList();
+
+protected:
+
+
+public:
+
+	[[nodiscard]]
+	at::Tensor GetBigramCounts() const noexcept;
+
+private:
+	TArray<FString> WordsList;
+
+	static bigramLookups bgl;
+};
