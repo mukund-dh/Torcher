6e5607a9febff10fdf1e26683ec37e1f7569c74e
Mukund Dhananjay <mukund.dh@outlook.com>
2024-11-15 20:38:55 +0530

    Fixed crash on initialization; Now using pointers to store and initialize the at::Tensors

--- Source/Torcher/Private/MakeMore/LinearLayer.cpp
+++ Source/Torcher/Private/MakeMore/LinearLayer.cpp
@@ -3,43 +3,95 @@
 
 #include "MakeMore/LinearLayer.h"
 
-#include "ATen/TensorIndexing.h"
-#include "ATen/core/interned_strings.h"
-
-TORCH_INCLUDES_START
-#include <torch/csrc/autograd/function.h>
-TORCH_INCLUDES_END
-
 ULinearLayer::ULinearLayer(const FObjectInitializer& ObjectInitializer) noexcept
 	: Super(ObjectInitializer)
 	, FanIn(27)
 	, FanOut(27)
+	, bHasBias(true)
+	, Seed(2147483647)
 {
-	Weights = NewObject<UObject>(GetTransientPackage(), UTorcherTensorFloat::StaticClass());
-	Bias = NewObject<UObject>(GetTransientPackage(), UTorcherTensorFloat::StaticClass());
+}
+
+ULinearLayer::~ULinearLayer() noexcept
+{
+	if (Weights != nullptr)
+		delete Weights;
+	
+	if (Bias != nullptr)
+		delete Bias;
+
+	if (Out != nullptr)
+		delete Out;
 }
 
 void ULinearLayer::InitTensors() noexcept
 {
 	auto opt = at::TensorOptions().dtype(c10::ScalarType::Float).device(c10::DeviceType::CPU);
-	at::Tensor RandWeights = torch::randn({FanIn, FanOut}, opt);
-	at::Tensor ZeroBiases = at::zeros({FanOut}, opt);
 
-	c10::IntArrayRef Sizes(RandWeights.sizes().data(), RandWeights.sizes().size());
+	// Are the weights currently valid?
+	if (!Weights)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Initializing weights to a normalized random tensor"));
+		auto gen = at::detail::createCPUGenerator(Seed);
+		
+		Weights = new at::Tensor(torch::randn({FanIn, FanOut}, gen, opt) / FMath::Pow(FanIn, 0.5));
+	}
 
-	auto* const TensorObject = NewObject<UObject>(GetTransientPackage(), UTorcherTensorFloat::StaticClass());
-	auto* const Tensor = CastChecked<ITorcherTensor>(TensorObject);
-
-	Tensor->SetDeviceType(ETorcherTensorDeviceType::Cpu);
-	Tensor->SetData(RandWeights);
-
-	Weights = TensorObject;
-
-	auto* const BiasTensorObject = NewObject<UObject>(GetTransientPackage(), UTorcherTensorFloat::StaticClass());
-	auto* const BiasTensor = CastChecked<ITorcherTensor>(BiasTensorObject);
-	BiasTensor->SetDeviceType(ETorcherTensorDeviceType::Cpu);
-	BiasTensor->SetData(ZeroBiases);
-
-	Bias = BiasTensorObject;
+	// Initialize biases to a zero tensor if this has biases.
+	if (bHasBias)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Initializing biases to a 0 tensor"));
+		Bias = new at::Tensor(at::zeros({FanOut}, opt));
+	}
 }
+
+at::Tensor ULinearLayer::Forward(const at::Tensor& InTensor) noexcept
+{
+	Out = new at::Tensor(torch::matmul(InTensor, *Weights));
+	if (Bias->defined())
+		*Out += *Bias;
+
+	return *Out;
+}
+
+float ULinearLayer::GetValueAtIndex(TArray<int32> IndexArray) const
+{
+	return Weights->data()[IndexArray[0]][IndexArray[1]].item<float>();
+}
+
+void ULinearLayer::GetParameters(TArray<float>& WeightVals, TArray<float>& Biases)
+{
+	// Extract values from Weights
+	if (Weights && Weights->defined())
+	{
+		std::vector<float> WeightsVec = ConvertTensorToVector(*Weights);
+		WeightVals.Append(&WeightsVec[0], WeightsVec.size());
+	}
+
+	// Extract values from Bias
+	if (Bias && Bias->defined())
+	{
+		std::vector<float> BiasVec = ConvertTensorToVector(*Bias);
+		Biases.Append(&BiasVec[0], BiasVec.size());
+	}
+}
+
+void ULinearLayer::GetOutputs(TArray<float>& OutVals)
+{
+	if (Out && Out->defined())
+	{
+		std::vector<float> OutVec = ConvertTensorToVector(*Out);
+		OutVals.Append(&OutVec[0], OutVec.size());
+	}
+}
+
+std::vector<float> ULinearLayer::ConvertTensorToVector(const at::Tensor& InTensor)
+{
+	return std::vector<float>(InTensor.data_ptr<float>(), InTensor.data_ptr<float>() + InTensor.numel());
+}
+
+/*at::Tensor ULinearLayer::ConvertArrayToTensor(const TArray<float>& Array, const std::vector<int64_t>& Sizes) const
+{
+}*/
+
 
--- Source/Torcher/Public/MakeMore/LinearLayer.h
+++ Source/Torcher/Public/MakeMore/LinearLayer.h
@@ -5,35 +5,75 @@
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
 #include "UObject/Package.h"
-#include "Tensors/TorcherTensorFloat.h"
 #include "Macros/TorcherPreProcMacros.h"
+
+TORCH_INCLUDES_START
+#include <torch/csrc/autograd/function.h>
+#include <torch/torch.h>
+TORCH_INCLUDES_END
 
 #include "LinearLayer.generated.h"
 
+
+
 /**
- * 
+ * Data Asset definition of a Linear Layer, which uses a float tensor
  */
 UCLASS(Blueprintable, BlueprintType)
 class TORCHER_API ULinearLayer : public UDataAsset
 {
 	GENERATED_BODY()
 
+public:
 	UE_NODISCARD_CTOR
 	ULinearLayer(const FObjectInitializer& ObjectInitializer) noexcept;
 
-public:
+	~ULinearLayer() noexcept;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Layer Inits")
+	// The Fan In for the tensor
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Layer|Inits")
 	int32 FanIn;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Layer Inits")
+	// The Fan Out for the tensor
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Layer|Inits")
 	int32 FanOut;
 
-	UObject* Weights;
-	UObject* Bias;
+	// Does this layer use it's bias?
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Layer|Params")
+	bool bHasBias;
+
+	// The seed for the generator
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Layer|Params")
+	int64 Seed;
+
+	// Tensor which stores the weights for this layer
+	at::Tensor* Weights;
+	
+	// Tensor to store bias(es) for this layer
+	at::Tensor* Bias;
+
+	// Tensor to store the output for this layer
+	at::Tensor* Out;
 
 public:
 
 	UFUNCTION(BlueprintCallable, Category = "Torcher|Tensor")
 	void InitTensors() noexcept;
+	
+	at::Tensor Forward(const at::Tensor& InTensor) noexcept;
+
+	UFUNCTION(BlueprintCallable, Category = "Torcher|Tensor")
+	float GetValueAtIndex(TArray<int32> IndexArray) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Torcher|Tensor Options")
+	void GetParameters(TArray<float>& WeightVals, TArray<float>& Biases);
+
+	UFUNCTION(BlueprintCallable, Category = "Torcher|Tensor Operations")
+	void GetOutputs(TArray<float>& OutVals);
+
+private:
+
+	std::vector<float> ConvertTensorToVector(const at::Tensor& InTensor);
+
+	// at::Tensor ConvertArrayToTensor(const TArray<float>& Array, const std::vector<int64_t>& Sizes) const;
 };
