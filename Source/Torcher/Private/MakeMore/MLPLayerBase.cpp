b18997923daddca95ffcf603ae90653f8385c2a6
Mukund Dhananjay <mukund.dh@outlook.com>
2024-11-19 22:35:38 +0530

    Adding in a base layer object so that I can extend it better to Linear, TanH and BatchNorm1d

---
+++ Source/Torcher/Private/MakeMore/MLPLayerBase.cpp
@@ -0,0 +1,123 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MakeMore/MLPLayerBase.h"
+
+
+UMLPLayerBase::UMLPLayerBase(const FObjectInitializer& ObjectInitializer) noexcept
+	: Super(ObjectInitializer)
+	, Out(nullptr)
+{
+}
+
+at::Tensor UMLPLayerBase::Forward(const at::Tensor& InTensor) noexcept
+{
+	// Just a basic implementation that takes in the passed Tensor
+	// and sets it as the OutTensor.
+	SetTensor(Out, InTensor);
+	return InTensor;
+}
+
+void UMLPLayerBase::SetTensor(at::Tensor*& TensorPtr, const at::Tensor& NewTensor) noexcept
+{
+	if (TensorPtr)
+	{
+		delete TensorPtr;
+		TensorPtr = nullptr;
+	}
+
+	TensorPtr = new at::Tensor(NewTensor.clone());
+}
+
+template <typename T>
+TArray<T> UMLPLayerBase::GetOutTensorAsArray() noexcept
+{
+	TArray<T> OutVec;
+	// Extract values from Out
+	if (Out && Out->defined())
+	{
+		if (std::is_same<T, int32>::value || std::is_same<T, int64>::value ||
+			std::is_same<T, float>::value || std::is_same<T, uint8>::value)
+		{
+			std::vector<T> WeightsVec = ConvertTensorToVector<T>(*Out);
+			OutVec.Append(&WeightsVec[0], WeightsVec.size());
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("Invalid Output Requested"));
+		}
+	}
+	return OutVec;
+}
+
+template <typename T>
+void UMLPLayerBase::SetTensor(at::Tensor*& TensorPtr, const TArray<T>& InArray, const TArray<int32>& Dimensions)
+{
+	if (TensorPtr)
+	{
+		delete TensorPtr;
+		TensorPtr = nullptr;
+	}
+
+	// Get the dimensions as an std::vector<int64_t> 
+	std::vector<int64_t> dims = std::vector<int64_t>(Dimensions.GetData(), Dimensions.GetData() + Dimensions.Num());
+
+	// Convert TArray to an std::Vector
+	std::vector<T> OutVec(InArray.GetData(), InArray.GetData() + InArray.Num());
+	
+	// Determine the type of Tensor T
+	at::TensorOptions options;
+	if (std::is_same<T, int32>::value || std::is_same<T, int64>::value)
+	{
+		options = at::TensorOptions().dtype(at::kInt).device(at::kCPU);
+	}
+	else if (std::is_same<T, float>::value)
+	{
+		options = at::TensorOptions().dtype(at::kFloat).device(at::kCPU);
+	}
+	else if (std::is_same<T, uint8>::value)
+	{
+		options = at::TensorOptions().dtype(at::kByte).device(at::kCPU);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Invalid TArray Type passed"));
+		return;
+	}
+
+	// Convert TArray to at::Tensor
+	TensorPtr = new at::Tensor(torch::from_blob(OutVec.data(), dims, options).clone());
+}
+
+template <typename T>
+std::vector<T> UMLPLayerBase::ConvertTensorToVector(const at::Tensor& InTensor)
+{
+	if (!std::is_same<T, int32>::value &&
+		!std::is_same<T, int64>::value &&
+		!std::is_same<T, float>::value &&
+		!std::is_same<T, uint8>::value)
+	{
+		// If the requested type is none of the supported types, return an empty
+		// vector of the passed type
+		UE_LOG(LogTemp, Error, TEXT("Invalid TArray Type Requested!"));
+		return std::vector<T>();
+	}
+	return std::vector<T>(InTensor.data_ptr<T>(), InTensor.data_ptr<T>() + InTensor.numel());
+}
+
+// Explicitly define possible templates for SetTensor
+template void UMLPLayerBase::SetTensor(at::Tensor*& TensorPtr, const TArray<int32>& InArray, const TArray<int32>& Dimensions);
+template void UMLPLayerBase::SetTensor(at::Tensor*& TensorPtr, const TArray<int64>& InArray, const TArray<int32>& Dimensions);
+template void UMLPLayerBase::SetTensor(at::Tensor*& TensorPtr, const TArray<float>& InArray, const TArray<int32>& Dimensions);
+template void UMLPLayerBase::SetTensor(at::Tensor*& TensorPtr, const TArray<uint8>& InArray, const TArray<int32>& Dimensions);
+
+// Explicitly define possible templates for ConvertTensorToVector
+template std::vector<int32> UMLPLayerBase::ConvertTensorToVector(const at::Tensor& InTensor);
+template std::vector<int64> UMLPLayerBase::ConvertTensorToVector(const at::Tensor& InTensor);
+template std::vector<float> UMLPLayerBase::ConvertTensorToVector(const at::Tensor& InTensor);
+template std::vector<uint8> UMLPLayerBase::ConvertTensorToVector(const at::Tensor& InTensor);
+
+// Explicitly define possible templates for GetOutTensorAsArray
+template TArray<int32> UMLPLayerBase::GetOutTensorAsArray() noexcept;
+template TArray<float> UMLPLayerBase::GetOutTensorAsArray() noexcept;
+template TArray<uint8> UMLPLayerBase::GetOutTensorAsArray() noexcept;
---
+++ Source/Torcher/Public/MakeMore/MLPLayerBase.h
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Macros/TorcherPreProcMacros.h"
+
+TORCH_INCLUDES_START
+#include <torch/csrc/autograd/function.h>
+#include <torch/torch.h>
+TORCH_INCLUDES_END
+
+#include "MLPLayerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class TORCHER_API UMLPLayerBase : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// Constructor
+	UE_NODISCARD_CTOR
+	UMLPLayerBase(const FObjectInitializer& ObjectInitializer) noexcept;
+
+	// Destructor
+	virtual ~UMLPLayerBase() noexcept
+	{
+		// Make sure we delete Out before we exit
+		if (Out)
+		{
+			delete Out;
+		}
+	}
+
+protected:
+	// Tensor to store the outputs for this layer.
+	at::Tensor* Out;
+
+public:
+	// Get the at::Tensor* for Out
+	[[nodiscard]]
+	FORCEINLINE at::Tensor* GetOut() const noexcept { return Out; }
+	
+	template <typename T>
+	TArray<T> GetOutTensorAsArray() noexcept;
+
+	UFUNCTION(BlueprintCallable, Category = "Torcher|Tensor Operations")
+	FORCEINLINE void GetOutTensorAsArray_Int32(TArray<int32>& OutArray) noexcept
+	{
+		OutArray = GetOutTensorAsArray<int32>();
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "Torcher|Tensor Operations")
+	FORCEINLINE void GetOutTensorAsArray_Float(TArray<float>& OutArray) noexcept
+	{
+		OutArray = GetOutTensorAsArray<float>();
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "Torcher|Tensor Operations")
+	FORCEINLINE void GetOutTensorAsArray_UInt8(TArray<uint8>& OutArray) noexcept
+	{
+		OutArray = GetOutTensorAsArray<uint8>();
+	}
+	
+	// Forward Pass on this layer
+	virtual at::Tensor Forward(const at::Tensor& InTensor) noexcept;
+
+protected:
+	// Set the value of the TensorPtr to the passed NewTensor
+	void SetTensor(at::Tensor*& TensorPtr, const at::Tensor& NewTensor) noexcept;
+
+	// Set the value of the TensorPtr to the passed TArray
+	template <typename T>
+	void SetTensor(at::Tensor*& TensorPtr, const TArray<T>& InArray, const TArray<int32>& Dimensions);
+
+	// Convert at::Tensor to a flat TArray<T>. This eschews the for-loop method, which would've
+	// taken a bit and a half to convert data. This is a slightly more direct model.
+	template <typename T>
+	std::vector<T> ConvertTensorToVector(const at::Tensor& InTensor);
+};
+
+
