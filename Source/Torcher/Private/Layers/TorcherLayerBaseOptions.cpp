a1b9d3aa9048cf60703c311a129af96bbb230533
Mukund Dhananjay <mukund.dh@outlook.com>
2024-11-27 11:38:17 +0530

    Adding the TorcherLayerBaseOptions

---
+++ Source/Torcher/Private/Layers/TorcherLayerBaseOptions.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Layers/TorcherLayerBaseOptions.h"
+
+#include "torch/nn/module.h"
+
+FTorcherLayerBaseOptions::FTorcherLayerBaseOptions() noexcept
+	: LayerName(TEXT("UNNAMED_LAYER"))
+{
+}
+
+void FTorcherLayerBaseOptions::SetLayerName(const torch::nn::Module& Module) noexcept
+{
+	LayerName = Module.name().c_str();
+}
---
+++ Source/Torcher/Public/Layers/TorcherLayerBaseOptions.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "TorcherLayerBaseOptions.generated.h"
+
+namespace torch::nn
+{
+	class Module;
+}
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType, DisplayName = "Torcher Layer Base Options")
+struct TORCHER_API FTorcherLayerBaseOptions
+{
+    GENERATED_BODY()
+
+	/*
+	 * Name of this layer
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess=true), Category = "Layer Params")
+	FString LayerName;
+
+	/*
+	 * Constructor
+	 */
+	UE_NODISCARD_CTOR
+	FTorcherLayerBaseOptions() noexcept;
+
+	/*
+	 * Sets the layer's name from a module
+	 */
+	void SetLayerName(const torch::nn::Module& Module) noexcept;
+};
