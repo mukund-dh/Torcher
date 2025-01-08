affbd91f585db260417b127bbb2db0b60be3a0a9
Mukund Dhananjay <mukund.dh@outlook.com>
2025-01-08 11:49:08 +0530

    Implemented BatchNorm1D

--- Source/Torcher/Private/Layers/Normalization/TorcherLayerBatchNorm1D.cpp
+++ Source/Torcher/Private/Layers/Normalization/TorcherLayerBatchNorm1D.cpp
@@ -2,3 +2,100 @@
 
 
 #include "Layers/Normalization/TorcherLayerBatchNorm1D.h"
+#include "BPUtils/TorcherTensorUtilities.h"
+#include "Tensors/TorcherTensorFloat.h"
+#include "UObject/Package.h"
+
+
+UTorcherLayerBatchNorm1D::UTorcherLayerBatchNorm1D(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+	, LayerDeviceType(ETorcherTensorDeviceType::Cpu)
+	, Dimensions(4)
+	, Eps(0.00001)
+	, Momentum(0.1)
+	, bIsTraining(true)
+{}
+
+void UTorcherLayerBatchNorm1D::InitializeLayerParams()
+{
+	Gamma = UTorcherTensorUtilities::CreateOnesTensor(UTorcherTensorFloat::StaticClass(), TArray<int64>{Dimensions}, LayerDeviceType);
+	Gamma->SetTensorLabel(TEXT("Gamma"));
+
+	Beta = UTorcherTensorUtilities::CreateZeroTensor(UTorcherTensorFloat::StaticClass(), TArray<int64>{Dimensions}, LayerDeviceType);
+	Beta->SetTensorLabel(TEXT("Beta"));
+
+	RunningMean = UTorcherTensorUtilities::CreateZeroTensor(UTorcherTensorFloat::StaticClass(), TArray<int64>{Dimensions}, LayerDeviceType);
+	RunningMean->SetTensorLabel(TEXT("RunningMean"));
+
+	RunningVariance = UTorcherTensorUtilities::CreateOnesTensor(UTorcherTensorFloat::StaticClass(), TArray<int64>{Dimensions}, LayerDeviceType);
+	RunningVariance->SetTensorLabel(TEXT("RunningVariance"));
+}
+
+TArray<TScriptInterface<ITorcherTensorBase>> UTorcherLayerBatchNorm1D::GetParameters() const
+{
+	TArray<TScriptInterface<ITorcherTensorBase>> Params;
+	Params.Add(Gamma);
+	Params.Add(Beta);
+	return Params;
+}
+
+bool UTorcherLayerBatchNorm1D::Forward(const TScriptInterface<ITorcherTensorBase>& Input,
+	TScriptInterface<ITorcherTensorBase>& Output) const
+{
+	at::Tensor* InputTensor = Input->GetData();
+	at::Tensor* XMean = RunningMean->GetData();
+	at::Tensor* XVar = RunningVariance->GetData();
+	if (bIsTraining)
+	{
+		*XMean = InputTensor->mean(0, true);
+		*XVar = InputTensor->var(0, true, true);
+	}
+
+	at::Tensor XHat = (*InputTensor - *XMean) / torch::sqrt(*XVar + Eps);
+	at::Tensor OutTensor = *Gamma->GetData() * XHat * *Beta->GetData();
+
+	// Update the buffers
+	if (bIsTraining)
+	{
+		torch::NoGradGuard NoGrad;
+		RunningMean->SetData((1 - Momentum) * (*RunningMean->GetData() + Momentum + *XMean));
+		RunningVariance->SetData((1 - Momentum) * (*RunningVariance->GetData() + Momentum + *XVar));
+	}
+
+	auto* const TensorObject = NewObject<UObject>(GetTransientPackage(), UTorcherTensorFloat::StaticClass());
+	auto* const Tensor = CastChecked<ITorcherTensorBase>(TensorObject);
+
+	Tensor->SetTensorDevice(LayerDeviceType);
+	Tensor->SetData(OutTensor);
+
+	Output = TensorObject;
+	return true;
+}
+
+bool UTorcherLayerBatchNorm1D::SetGradientToZero(bool bSetToNone)
+{
+	if (Gamma->GetData()->grad().defined())
+	{
+		Gamma->SetGradientToZero();
+	}
+
+	if (Beta->GetData()->grad().defined())
+	{
+		Beta->SetGradientToZero();
+	}
+	return true;
+}
+
+void UTorcherLayerBatchNorm1D::CloneData(TScriptInterface<ITorcherTensorBase>& OutClone, UObject* Outer)
+{
+}
+
+void UTorcherLayerBatchNorm1D::SetLayerDeviceType(ETorcherTensorDeviceType DeviceType)
+{
+	LayerDeviceType = DeviceType;
+	Gamma->SetTensorDevice(DeviceType);
+	Beta->SetTensorDevice(DeviceType);
+	
+	RunningVariance->SetTensorDevice(DeviceType);
+	RunningMean->SetTensorDevice(DeviceType);
+}
--- Source/Torcher/Public/Layers/Normalization/TorcherLayerBatchNorm1D.h
+++ Source/Torcher/Public/Layers/Normalization/TorcherLayerBatchNorm1D.h
@@ -10,8 +10,119 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable, BlueprintType, DisplayName = "Torcher BatchNorm1D Layer")
 class TORCHER_API UTorcherLayerBatchNorm1D : public UObject, public IITorcherLayer
 {
 	GENERATED_BODY()
+
+public:
+	/*
+	 * Constructor
+	 */
+	UE_NODISCARD_CTOR
+	UTorcherLayerBatchNorm1D(const FObjectInitializer& ObjectInitializer);
+
+	/*
+	 * The Layer Device Type
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	ETorcherTensorDeviceType LayerDeviceType;
+
+	/*
+	 * The Gamma Tensor. Parameters of the layer.
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	TScriptInterface<ITorcherTensorBase> Gamma;
+
+	/*
+	 * The Beta Tensor. Parameters of the layer.
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	TScriptInterface<ITorcherTensorBase> Beta;
+
+	/*
+	 * The Running Mean Tensor. Part of buffers, trained with running momentum update.
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	TScriptInterface<ITorcherTensorBase> RunningMean;
+
+	/*
+	 * The Running Variance Tensor. Part of buffers, trained with running momentum update.
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	TScriptInterface<ITorcherTensorBase> RunningVariance;
+
+	/*
+	 * Dimensions of the Weights Tensor.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	int64 Dimensions;
+	
+	/*
+	 * Epsilon value. Constant for a layer.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	float Eps;
+
+	/*
+	 * Momentum of the normalization.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	float Momentum;
+
+protected:
+	/*
+	 * Is this training?
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "BatchNorm1D Layer|Params")
+	bool bIsTraining;
+
+public:
+
+	/*
+	 * Initialize the Parameters of this Layer. In this case, they are the Weights and Bias Tensors.
+	 * Weights are a torch::randn, bias is a torch::zeros
+	 */
+	virtual void InitializeLayerParams() override;
+
+	/*
+	 * Get the Parameters of this layer as an array
+	 *
+	 * @return An array of TorcherTensors
+	 */
+	virtual TArray<TScriptInterface<ITorcherTensorBase>> GetParameters() const override;
+
+	/*
+	 * Forward pass of the Linear Layer. Does a torch::matmul on Weights & the input.
+	 * Adds in the bias if bUseBias is set to true.
+	 *
+	 * @param Input The tensor to run the forward pass with
+	 * @param Output The resultant tensor of the forward pass
+	 * @return Was this operation successful
+	 */
+	virtual bool Forward(const TScriptInterface<ITorcherTensorBase>& Input, TScriptInterface<ITorcherTensorBase>& Output) const override;
+
+	/*
+	 * Set the gradients of the Bias and Weights tensors to 0
+	 *
+	 * @param bSetToNone Set the gradient to none if this is set to true
+	 * @return Was this operation successful?
+	 */
+	virtual bool SetGradientToZero(bool bSetToNone) override;
+
+	/*
+	 * Clone the Weights Tensor
+	 *
+	 * @param OutClone The Output cloned tensor.
+	 * @param Outer The outer object of the tensor.
+	 */
+	virtual void CloneData(TScriptInterface<ITorcherTensorBase>& OutClone, UObject* Outer) override;
+
+	/*
+	 * Set the layer's device type, and by extension, the device type of all the parameters
+	 * of this layer to provided input
+	 *
+	 * @param DeviceType The Tensor device type to use.
+	 */
+	virtual void SetLayerDeviceType(ETorcherTensorDeviceType DeviceType) override; 
 };
